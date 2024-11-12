bea51f4a81274c75b9bad05da5bb9050490a5084
west_east <west.east1832@gmail.com>
2024-11-12 16:25:15 +0900

    [fix]: 시큐리티 최신화

--- src/main/java/kr/moonwalk/moonwalk_api/config/SecurityConfig.java
+++ src/main/java/kr/moonwalk/moonwalk_api/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,7 +39,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(PERMIT_URLS.toArray(new String[0])).permitAll()
                 .anyRequest().authenticated())
