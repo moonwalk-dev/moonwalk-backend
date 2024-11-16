4fb146fc157122b591ce2c5801f0ddd8302331d0
west_east <west.east1832@gmail.com>
2024-11-17 00:23:42 +0900

    [chore]: 회원가입 로그인 기본값 설정

--- src/main/java/kr/moonwalk/moonwalk_api/dto/auth/UserLoginDto.java
+++ src/main/java/kr/moonwalk/moonwalk_api/dto/auth/UserLoginDto.java
@@ -1,15 +1,18 @@
 package kr.moonwalk.moonwalk_api.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserLoginDto {
 
-    @NotBlank(message = "아이디는 필수입니다.")
+    @NotBlank(message = "이메일 필수입니다.")
+    @Schema(description = "이메일", defaultValue = "moonwalk@gmail.com")
     private String email;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
+    @Schema(description = "비밀번호", defaultValue = "moonwalk123")
     private String password;
 
 }
--- src/main/java/kr/moonwalk/moonwalk_api/dto/auth/UserRegistrationDto.java
+++ src/main/java/kr/moonwalk/moonwalk_api/dto/auth/UserRegistrationDto.java
@@ -8,13 +8,13 @@ import lombok.Data;
 @Data
 public class UserRegistrationDto {
 
-    @NotBlank(message = "아이디는 필수입니다.")
-    @Schema(description = "아이디")
+    @NotBlank(message = "이메일 필수입니다.")
+    @Schema(description = "이메일", defaultValue = "moonwalk@gmail.com")
     private String email;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
-    @Schema(description = "비밀번호")
+    @Schema(description = "비밀번호", defaultValue = "moonwalk123")
     private String password;
 
 }
