d05f9a934bd812be4ad0c5883fe5beb244f13be7
west_east <west.east1832@gmail.com>
2024-11-29 09:46:39 +0900

    [feat]: 모듈 카테고리 데이터 추가

--- src/main/java/kr/moonwalk/moonwalk_api/config/loader/CategoryLoader.java
+++ src/main/java/kr/moonwalk/moonwalk_api/config/loader/CategoryLoader.java
@@ -1,5 +1,7 @@
 package kr.moonwalk.moonwalk_api.config.loader;
 
+import java.util.ArrayList;
+import java.util.List;
 import kr.moonwalk.moonwalk_api.domain.Category;
 import kr.moonwalk.moonwalk_api.domain.Category.Type;
 import kr.moonwalk.moonwalk_api.repository.CategoryRepository;
@@ -27,8 +29,31 @@ public class CategoryLoader implements CommandLineRunner {
 
             String[] moduleCategory = {"공용공간", "사무공간", "출입공간", "회의공간"};
 
+            List<Category> parentCategoryList = new ArrayList<>();
             for (String categoryName : moduleCategory) {
                 Category category = new Category(categoryName, null, Type.TYPE_MODULE);
+                categoryRepository.save(category);
+                parentCategoryList.add(category);
+            }
+
+            List<Category> subCategoryList = new ArrayList<>();
+
+            subCategoryList.add(new Category("라운지", parentCategoryList.get(0), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("락커룸", parentCategoryList.get(0), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("팬트리", parentCategoryList.get(0), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("휴게공간", parentCategoryList.get(0), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("업무공간", parentCategoryList.get(1), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("임원실", parentCategoryList.get(1), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("집중업무공간", parentCategoryList.get(1), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("OA공간", parentCategoryList.get(1), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("이미지월", parentCategoryList.get(2), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("인포메이션", parentCategoryList.get(2), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("대회의실", parentCategoryList.get(3), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("소회의실", parentCategoryList.get(3), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("중회의실", parentCategoryList.get(3), Type.TYPE_MODULE));
+            subCategoryList.add(new Category("퀵미팅부스", parentCategoryList.get(3), Type.TYPE_MODULE));
+
+            for (Category category : subCategoryList) {
                 categoryRepository.save(category);
             }
         }
