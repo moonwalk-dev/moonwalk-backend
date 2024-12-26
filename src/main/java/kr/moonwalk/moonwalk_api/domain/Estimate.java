e27e8dc1e3ed334e0771038f4000fa3aaa4f0076
west_east <west.east1832@gmail.com>
2024-12-26 13:30:06 +0900

    [fix]: add estimate mood ManyToOne

--- src/main/java/kr/moonwalk/moonwalk_api/domain/Estimate.java
+++ src/main/java/kr/moonwalk/moonwalk_api/domain/Estimate.java
@@ -5,13 +5,13 @@ import static jakarta.persistence.FetchType.LAZY;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -30,7 +30,7 @@ public class Estimate {
     @Column(name = "estimate_id")
     private Long id;
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mood_id")
     private Mood mood;
 
