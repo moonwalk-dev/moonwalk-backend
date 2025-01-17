47a9b37fd3f3b4dbced9dab1fcc9c40fe4af3954
서동현 <108206977+west-eastH@users.noreply.github.com>
2025-01-17 15:42:43 +0900

    Update ModuleResponseDto.java

--- src/main/java/kr/moonwalk/moonwalk_api/dto/module/ModuleResponseDto.java
+++ src/main/java/kr/moonwalk/moonwalk_api/dto/module/ModuleResponseDto.java
@@ -5,27 +5,34 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class ModuleResponseDto {
-
     private Long id;
-
     private String name;
-
     private String description;
-
     private String size;
-
-    private int price;
-
+    private String price;
     private List<String> materials;
-
     private String serialNumber;
-
     private int capacity;
-
     private String topImageUrl;
-
     private String isoImageUrl;
 
+    public ModuleResponseDto(Long id, String name, String description, String size, 
+            int price, List<String> materials, String serialNumber, 
+            int capacity, String topImageUrl, String isoImageUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.size = size;
+        this.price = formatPrice(price);
+        this.materials = materials;
+        this.serialNumber = serialNumber;
+        this.capacity = capacity;
+        this.topImageUrl = topImageUrl;
+        this.isoImageUrl = isoImageUrl;
+    }
+
+    private String formatPrice(int price) {
+        return NumberFormat.getNumberInstance().format(price);
+    }
 }
