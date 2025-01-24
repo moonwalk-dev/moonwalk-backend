6478a42e8e4c6da49e86a13e5dfc369e3e5061b3
west_east <west.east1832@gmail.com>
2025-01-24 14:05:22 +0900

    fix projects price format

--- src/main/java/kr/moonwalk/moonwalk_api/dto/mypage/ProjectInfoDto.java
+++ src/main/java/kr/moonwalk/moonwalk_api/dto/mypage/ProjectInfoDto.java
@@ -2,27 +2,31 @@ package kr.moonwalk.moonwalk_api.dto.mypage;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class ProjectInfoDto {
-
     private Long id;
-
     private String title;
-
     private String client;
-
     private String area;
-
-    private int estimatedTotalPrice;
-
-    private int placedTotalPrice;
+    private String estimatedTotalPrice;
+    private String placedTotalPrice;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime createdAt;
-
     private String coverImageUrl;
+
+    public ProjectInfoDto(Long id, String title, String client, String area,
+        int estimatedTotalPrice, int placedTotalPrice,
+        LocalDateTime createdAt, String coverImageUrl) {
+        this.id = id;
+        this.title = title;
+        this.client = client;
+        this.area = area;
+        this.estimatedTotalPrice = String.format("%,d", estimatedTotalPrice);
+        this.placedTotalPrice = String.format("%,d", placedTotalPrice);
+        this.createdAt = createdAt;
+        this.coverImageUrl = coverImageUrl;
+    }
 }
